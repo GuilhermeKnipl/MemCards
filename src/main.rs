868afc4a89cb42ca62d439099d9865e29c366a40
GuilhermeKnipl <guirissatti@gmail.com>
2025-04-25 13:40:52 -0300

    Starting Connection to db

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use clap::Parser;
 use mem_cards::client::Cli;
-
+use mem_cards::database;
 
 fn main() {
+
+    let conn = database::establish_connection();
+    //conn    
+
     let cli: Cli= Cli::parse();
 
     if let Err(e) = cli.run() {
@@ -10,4 +14,4 @@ fn main() {
         std::process::exit(1);
     
     }
-}
+
