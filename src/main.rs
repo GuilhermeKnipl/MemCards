55f946e3a8930fc4be182ffc42d59cc1b646a25b
GuilhermeKnipl <guirissatti@gmail.com>
2025-04-26 10:56:50 -0300

    add word and record debugging

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use clap::Parser;
+//use colored::Colorize;
 use mem_cards::client::Cli;
-use mem_cards::database;
+//use mem_cards::misc::now_datetime;
+use mem_cards::sql::{mQuery,mConnect};
 
 fn main() {
 
-    let conn = database::establish_connection();
+    let conn = mConnect::establish_connection();
+    
+    //mQuery::add_word(&conn, "test2", "thanksa","adas");
+    println!("{:?}",mQuery::word_exist(&conn, "test2"));
+    println!("{:?}" ,mQuery::word_exist(&conn, "test32"));
+    //mQuery::get_records(conn, "test2",false).unwrap();
+    //println!("{}", creation_date.bold());
     //conn    
 
     let cli: Cli= Cli::parse();
@@ -12,6 +20,7 @@ fn main() {
     if let Err(e) = cli.run() {
         eprintln!("Error {}", e);
         std::process::exit(1);
-    
+
     }
+}
 
