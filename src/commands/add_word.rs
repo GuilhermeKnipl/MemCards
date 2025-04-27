67b7ac7c3e6a463471df9380c896239f5f97dd78
GuilhermeKnipl <guirissatti@gmail.com>
2025-04-27 17:51:25 -0300

    Add & Updtade word command working

--- src/commands/add_word.rs
+++ src/commands/add_word.rs
@@ -1,37 +1,59 @@
 use std::io::{self, Write};
 use colored::*;
+use crate::sql::mConnect::establish_connection;
+use crate::sql::mQuery::{self, word_exist};
+//use crate::misc::now_datetime;
+
 
 
 
 pub fn run(){
+
     let fail_inpt: &str = "Fail to read your input";    
 
-    print!("\n{}: ", "Add a new word".trim().yellow().bold());
-
-    io::stdout().flush().unwrap();
+    let conn = establish_connection();
 
     let mut new_word: String = String::new();
+    let mut pred_word : String = String::new();
+    let mut context : String = String::new();
+
+
+    print!("\n{}: ", "# Add a new word".trim().white().bold());
+    io::stdout().flush().unwrap();
     io::stdin()
         .read_line(&mut new_word)
         .expect(fail_inpt);
 
-    print!("\n{} ", format!("Add a meaning to the word:").trim().yellow().bold());
 
+
+    print!("{} ", format!("## Add a meaning to the word:").trim().white().bold());
     io::stdout().flush().unwrap();
-
-    let mut pred_word : String = String::new();
-    
     io::stdin()
         .read_line(&mut pred_word)
         .expect(fail_inpt);
 
-    println!("{}", format!("{} = {}"
-        , new_word.trim().bright_blue().bold()
-        , pred_word.trim().green().bold()).bold());  
+
+    print!("{} ", format!("### Add a context to the word(OPTIONAL):").trim().white().bold());
+    io::stdout().flush().unwrap();
+    io::stdin()
+        .read_line(&mut context)
+        .expect(fail_inpt);
+
+
+
+
+    if word_exist(&conn, &new_word){
+        println!("\n Old Meaning: \n New Meaning");
+
+    } else {
+        println!("{}",format!("New word {} Added To Memory", &new_word.trim()).green().bold());
+    }
+
+    mQuery::add_word(&conn, &new_word, &pred_word, &context);
+
+    mQuery::get_records(&conn, &new_word, true).unwrap();
     
+    conn.close().unwrap();
 
-    //println!("{}", format!("Input is {}", input).bold().blue())
-
-    // real_word
 }
 
