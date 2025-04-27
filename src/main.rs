cade80a95ca150d047e6b937a0ad16819ff06438
GuilhermeKnipl <guirissatti@gmail.com>
2025-04-27 20:37:12 -0300

    README

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,9 @@ use clap::Parser;
 //use colored::Colorize;
 use mem_cards::client::Cli;
 //use mem_cards::misc::now_datetime;
-use mem_cards::sql::{mQuery,mConnect};
+//use mem_cards::sql::{mQuery,mConnect};
 
 fn main() {
-
-    let conn = mConnect::establish_connection();
-    
-    //mQuery::add_word(&conn, "test2", "thanksa","adas");
-    println!("{:?}",mQuery::word_exist(&conn, "test2"));
-    println!("{:?}" ,mQuery::word_exist(&conn, "test32"));
-    //mQuery::get_records(conn, "test2",false).unwrap();
-    //println!("{}", creation_date.bold());
-    //conn    
 
     let cli: Cli= Cli::parse();
 
