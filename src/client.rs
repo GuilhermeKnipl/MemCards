f96e531684fb65ba4e9c85b7cd70c1eb5a70a22e
GuilhermeKnipl <guirissatti@gmail.com>
2025-04-27 22:06:44 -0300

    New commands Add, Search, Backup

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,5 @@
+use std::u8;
+
 use clap::{Parser, Subcommand};
 use crate::commands;
 
@@ -10,13 +12,32 @@ pub struct Cli {
 
 #[derive(Subcommand)]
 pub enum Commands {
-    Greet {
+    Stats {
         name: String,
         #[arg(short, long, default_value_t = 1)]
         count: u8,
     },
     Add {
     
+    },
+    Search{
+        word: String,
+
+        #[arg(short = 's', long = "sort-asc" ,default_value_t = false, help = "Sorting Ascending (default = Desc)")]
+        sort_asc: bool,
+        
+        #[arg(short = 'l', long = "limit" ,default_value_t = 10, help = "Limit results")]
+        limit: u8,
+
+        #[arg(short = 'a', long = "all" ,default_value_t = false, help= "List all records")]
+        records: bool,
+
+        #[arg(short = 'd', long = "detail" ,default_value_t = false, help= "Show Word Info")]
+        detail: bool
+    },
+
+    Backup {
+        end_path: String
     }
 
 }
@@ -24,11 +45,17 @@ pub enum Commands {
 impl Cli {
     pub fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
         match &self.command {
-            Commands::Greet { name, count } => {
-                commands::greet::run(name, *count);
+            Commands::Stats { name, count } => {
+                commands::stats::run(name, *count);
             }
             Commands::Add { } => {
                 commands::add_word::run();
+            }
+            Commands::Search { word, sort_asc, limit, records, detail} => {
+                commands::search_word::run(word, sort_asc, limit, records, detail);
+            }
+            Commands::Backup { end_path } => {
+                commands::backup::run(end_path);
             }
         }
         Ok(())
