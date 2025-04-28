2170333f59230cdfd9b18cd5c918408609ee127f
GuilhermeKnipl <guirissatti@gmail.com>
2025-04-27 22:02:17 -0300

    Time formatting for file name

--- src/misc.rs
+++ src/misc.rs
@@ -5,3 +5,8 @@ pub fn now_datetime() -> String{
     let now_dt = Local::now().format("%Y-%m-%d %H:%M:%S");
     return now_dt.to_string()
 }
+
+pub fn now_date_file_fmt() -> String {
+    let now_fmt = Local::now().format("%Y_%m_%d");
+    return now_fmt.to_string();
+}
